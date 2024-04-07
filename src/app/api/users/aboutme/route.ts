ba0fd7ba51c8fa69566937378ddefa6f2e773713
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-04-07 22:23:16 +0530

    Modified

--- src/app/api/users/aboutme/route.ts
+++ src/app/api/users/aboutme/route.ts
@@ -1,10 +1,26 @@
 import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect();
 
 export async function POST(request: NextRequest) {
-    // retrive data from token
-    const userId = await getDataFromToken(request)
+  // retrive data from token
+  const userId = await getDataFromToken(request);
+  const user = await User.findOne({ _id: userId }).select("-password");
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        error: "Invalid token",
+      },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json({
+    message: "User found",
+    data: user,
+  });
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function page() {
-  return (
-    <div>page</div>
-  )
+  const router = useRouter();
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed!");
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user.username.length > 0 && user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false)
+    } else {
+      setButtonDisabled(true)
+    }
+  }, [user])
+
+  return <div>
+    <h1>{loading ? "Processing" : "Signup"}</h1>
+    <label htmlFor="username">Username: </label>
+    <input type="text" required />
+  </div>;
 }
